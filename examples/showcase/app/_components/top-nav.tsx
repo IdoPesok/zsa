56ed1ea470926f66225d3361f965a9a1056113ef
Wes Convery <2wconvery@gmail.com>
2024-05-19 19:46:03 -0700

    simple docs updates

--- examples/showcase/app/_components/top-nav.tsx
+++ examples/showcase/app/_components/top-nav.tsx
@@ -4,7 +4,7 @@ import { ThemeToggle } from "@/components/theme/theme-toggle"
 import { Button } from "@/components/ui/button"
 import { getDocPosts } from "@/lib/docs"
 import { cn } from "@/lib/utils"
-import { Coffee, GithubIcon, TwitterIcon } from "lucide-react"
+import { Clapperboard, Coffee, GithubIcon, Network, TwitterIcon } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
@@ -19,8 +19,8 @@ export default function TopNav({
     <div className="sticky h-fit top-0 bg-background/80 backdrop-blur z-20  overscroll-none">
       <div className="border-b border-muted flex flex-row gap-10 items-center w-full px-10 max-w-screen py-4">
         <h3 className="whitespace-nowrap font-bold flex gap-3 items-center">
-          <Coffee className="size-5" />
-          zsa
+          <Network className="size-5" />
+          Zod Server Actions
         </h3>
         <div className="flex-1 flex-row gap-8 items-center px-4  text-sm font-medium">
           <Link
@@ -28,7 +28,7 @@ export default function TopNav({
             className={cn(
               "border-b-2 py-2 border-transparent hover:border-muted text-muted-foreground hover:text-foreground",
               !pathname.includes("/actions/") &&
-                "border-primary hover:border-primary text-foreground"
+              "border-primary hover:border-primary text-foreground"
             )}
           >
             Documentation
@@ -36,23 +36,13 @@ export default function TopNav({
         </div>
         <div className="flex justify-end gap-2 items-center">
           <Link
-            href="https://github.com/"
+            href="https://github.com/IdoPesok/zsa"
             passHref
             target="_blank"
             rel="noopener"
           >
             <Button size={"icon"} variant={"outline"}>
               <GithubIcon className="h-4 w-4" />
-            </Button>
-          </Link>
-          <Link
-            href="https://twitter.com/"
-            passHref
-            target="_blank"
-            rel="noopener"
-          >
-            <Button size={"icon"} variant={"outline"}>
-              <TwitterIcon className="h-4 w-4" />
             </Button>
           </Link>
           <ThemeToggle />
