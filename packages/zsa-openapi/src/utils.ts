3f6049ca14793230060c2b01b927dc54ed20106d
Ido Pesok <idopesok91@gmail.com>
2024-05-28 21:22:36 -0700

    added more error types

--- packages/zsa-openapi/src/utils.ts
+++ packages/zsa-openapi/src/utils.ts
@@ -45,6 +45,24 @@ export const getErrorStatusFromZSAError = (error: unknown) => {
       return 401
     case "TIMEOUT":
       return 408
+    case "FORBIDDEN":
+      return 403
+    case "NOT_FOUND":
+      return 404
+    case "CONFLICT":
+      return 409
+    case "PRECONDITION_FAILED":
+      return 412
+    case "PAYLOAD_TOO_LARGE":
+      return 413
+    case "METHOD_NOT_SUPPORTED":
+      return 405
+    case "UNPROCESSABLE_CONTENT":
+      return 422
+    case "TOO_MANY_REQUESTS":
+      return 429
+    case "CLIENT_CLOSED_REQUEST":
+      return 499
     default:
       return 500
   }
