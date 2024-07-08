f0f9578b1e06a921dfbc8c9b4258c2199dc88cbc
Ido Pesok <idopesok91@gmail.com>
2024-07-07 22:12:29 -0700

    more side effects

--- packages/zsa/src/router.ts
+++ packages/zsa/src/router.ts
@@ -1,11 +1,17 @@
 import { TAnyZodSafeFunctionHandler } from "./types"
 import {
+  inferServerActionError,
   inferServerActionInput,
+  inferServerActionReturnData,
   inferServerActionReturnType,
 } from "./zod-safe-function"
 
 interface TAnyActionMap {
   [key: string]: TAnyZodSafeFunctionHandler
+}
+
+interface TAnyRouterMap {
+  [key: string]: TAnyServerActionRouterFn
 }
 
 export interface TServerActionRouterFn<
@@ -27,6 +33,22 @@ type TOnStart<TActionMap extends TAnyActionMap> = {
   }
 }[keyof TActionMap]
 
+type TOnError<TActionMap extends TAnyActionMap> = {
+  [TOnStartKey in keyof TActionMap]: {
+    key: TOnStartKey
+    input: inferServerActionInput<TActionMap[TOnStartKey]>
+    error: inferServerActionError<TActionMap[TOnStartKey]>
+  }
+}[keyof TActionMap]
+
+type TOnSuccess<TActionMap extends TAnyActionMap> = {
+  [TOnStartKey in keyof TActionMap]: {
+    key: TOnStartKey
+    input: inferServerActionInput<TActionMap[TOnStartKey]>
+    data: inferServerActionReturnData<TActionMap[TOnStartKey]>
+  }
+}[keyof TActionMap]
+
 type TOnComplete<TActionMap extends TAnyActionMap> = {
   [TOnCompleteKey in keyof TActionMap]: {
     key: TOnCompleteKey
@@ -45,10 +67,14 @@ export const createServerActionRouter = <
   opts?: {
     onStart?: (args: TOnStart<TActionMap>) => void
     onComplete?: (args: TOnComplete<TActionMap>) => void
+    onError?: (args: TOnError<TActionMap>) => void
+    onSuccess?: (args: TOnSuccess<TActionMap>) => void
   }
 ): TServerActionRouterFn<TActionMap, keyof TActionMap> => {
   const routerFn = async <TActionKey extends keyof TActionMap>(
+    /** The key of the action to execute */
     key: TActionKey,
+    /** The input to the action */
     ...args: Parameters<TActionMap[TActionKey]>
   ): Promise<ReturnType<TActionMap[TActionKey]>> => {
     await opts?.onStart?.({ key, input: args[0] })
@@ -56,6 +82,16 @@ export const createServerActionRouter = <
     const action = actionMap[key]
     // @ts-expect-error
     const result = await action(...args)
+
+    if (result) {
+      const [data, err] = result
+
+      if (err) {
+        await opts?.onError?.({ key, input: args[0], error: err })
+      } else {
+        await opts?.onSuccess?.({ key, input: args[0], data })
+      }
+    }
 
     await opts?.onComplete?.({ key, input: args[0], result: result as any })
 
@@ -66,5 +102,6 @@ export const createServerActionRouter = <
   return routerFn
 }
 
-export type inferMapFromServerActionRouterFn<T> =
-  T extends TServerActionRouterFn<infer TActionMap, any> ? TActionMap : never
+export type inferMapFromServerActionRouterFn<
+  T extends TAnyServerActionRouterFn,
+> = T extends TServerActionRouterFn<infer TActionMap, any> ? TActionMap : never
